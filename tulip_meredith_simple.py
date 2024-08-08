0b59651ffead23849f274107304c050b6afeeb27
a-s-russo <110582146+a-s-russo@users.noreply.github.com>
2024-08-08 11:37:12 +0930

    Update tulip_meredith_simple.py

--- tulip_meredith_simple.py
+++ tulip_meredith_simple.py
@@ -1,38 +1,41 @@
 def tulip_coin_strategy(history):
-    current_price = history.iloc[-1]['price_history']
-    minutes_remaining = history.iloc[-1]['minutes_remaining']
-    proportion=0.0
-
-    if minutes_remaining>=53:       #at the first opportunity you get, buy everything as it is surely the cheapest it will be. Surely there will be a chance to buy in the first 7 minutes.
-        decision='buy'
-        proportion=1
+    if not history.empty:
+        current_price = history.iloc[-1]['price_history']
+        minutes_remaining = history.iloc[-1]['minutes_remaining']
+        proportion=0.0
     
-    elif 10<=minutes_remaining<53:
-        if current_price>6:
-            decision='sell'
-            proportion=0.20
-        elif current_price<4:
+        if minutes_remaining>=53:       #at the first opportunity you get, buy everything as it is surely the cheapest it will be. Surely there will be a chance to buy in the first 7 minutes.
             decision='buy'
-            proportion=0.20
-        else: 
-            decision='hold'
             proportion=1
+        
+        elif 10<=minutes_remaining<53:
+            if current_price>6:
+                decision='sell'
+                proportion=0.20
+            elif current_price<4:
+                decision='buy'
+                proportion=0.20
+            else: 
+                decision='hold'
+                proportion=1
+        
+        elif 5<=minutes_remaining<10:
+            if current_price>5:
+                decision='sell'
+                proportion=1
+            else: 
+                decision='hold'
+                proportion=1
     
-    elif 5<=minutes_remaining<10:
-        if current_price>5:
-            decision='sell'
-            proportion=1
-        else: 
-            decision='hold'
-            proportion=1
-
-    elif 0<=minutes_remaining<5:
-        if current_price>4:
-            decision='sell'
-            proportion=1
-        else:
-            decision='hold'
-            proportion=1
-
-    return decision, proportion
+        elif 0<=minutes_remaining<5:
+            if current_price>4:
+                decision='sell'
+                proportion=1
+            else:
+                decision='hold'
+                proportion=1
+    
+        return decision, proportion
+        
+    return 'hold', 1
 
